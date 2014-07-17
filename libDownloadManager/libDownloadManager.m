503582752e877ebe6939de0bf8aee4653d14fa99
Denis Martin <denis@irlmobile.com>
2014-07-17 08:29:27 +0200

    - Workign copy denis

---
+++ libDownloadManager/libDownloadManager.m
@@ -0,0 +1,13 @@
+//
+//  libDownloadManager.m
+//  libDownloadManager
+//
+//  Created by Denis Martin on 03/04/2014.
+//  Copyright (c) 2014 InfusionApps. All rights reserved.
+//
+
+#import "libDownloadManager.h"
+
+@implementation libDownloadManager
+
+@end
